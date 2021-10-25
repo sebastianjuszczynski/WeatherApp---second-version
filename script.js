232225eedf2ff1dfb383d1d7812209b983976a60
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2021-10-25 17:54:15 +0100

    all functionality added

--- script.js
+++ script.js
@@ -12,18 +12,41 @@ const API_KEY = "&appid=4135e4d5d7091f795d21a640604abdd9";
 const API_UNITS = "&units=metric";
 
 const getWeather = () => {
-    const city = input.value.trim() || "London";
+    const city = input.value.trim()
     const URL = API_LINK + city + API_KEY + API_UNITS
 
     axios.get(URL).then(res => {
-        console.log(res.data)
         const temp = res.data.main.temp
         const hum = res.data.main.humidity
+        const status = Object.assign({}, ...res.data.weather)
+
         cityName.textContent = res.data.name
         temperature.textContent = Math.floor(temp) + "°C"
         humidity.textContent = hum + "%"
+        weather.textContent = status.main
+
+        warning.textContent = ""
+        input.value = ""
         
-    })
+        if (status.id >= 200 && status.id < 300) {
+            photo.setAttribute("src", "./img/thunderstorm.png")
+        } else if (status.id >= 300 && status.id < 400) {
+            photo.setAttribute("src", "./img/drizzle.png")
+        } else if (status.id >= 500 && status.id < 600) {
+            photo.setAttribute("src", "./img/rain.png")
+        } else if (status.id >= 600 && status.id < 700) {
+            photo.setAttribute("src", "./img/ice.png")
+        } else if (status.id >= 700 && status.id < 800) {
+            photo.setAttribute("src", "./img/fog.png")
+        }  else if (status.id >= 801 && status.id < 900) {
+            photo.setAttribute("src", "./img/cloud.png")
+        } else if (status.id === 800){
+            photo.setAttribute("src", "./img/sun.png")
+        } else {
+            photo.setAttribute("src", "./img/unknown.png")
+        }
+        
+    }).catch(() => warning.textContent = "Type a correct city name!")
 
 }
-getWeather();
+button.addEventListener("click", getWeather);
